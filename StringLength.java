232195f8405e44335bac93ad9c133d4b05dc7e53
Yash Ahlawat <ya7548@srmist.edu.in>
2026-05-02 22:10:07 +0530

    Q1

---
+++ StringLength.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class StringLength {
+    static int getLength(String s) {
+        int count = 0;
+        try {
+            while (true) {
+                s.charAt(count);
+                count++;
+            }
+        } catch (Exception e) {
+            return count;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(getLength(s));
+        System.out.println(s.length());
+    }
+}
