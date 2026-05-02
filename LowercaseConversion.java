89efde07a4447cbadd493fb7d3f27e9ce53bbc20
Yash Ahlawat <ya7548@srmist.edu.in>
2026-05-02 22:09:18 +0530

    Q10

---
+++ LowercaseConversion.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class LowercaseComparison {
+    static String lower(String s) {
+        String r = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c >= 'A' && c <= 'Z') c = (char)(c + 32);
+            r += c;
+        }
+        return r;
+    }
+
+    static boolean compare(String a, String b) {
+        if (a.length() != b.length()) return false;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String a = lower(s);
+        String b = s.toLowerCase();
+        System.out.println(compare(a, b));
+    }
+}
